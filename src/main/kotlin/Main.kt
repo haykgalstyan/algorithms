fa30af64f4787c884a0bd2b5d680da64c10d102c
Hayk Galstyan <hayk.galstyan.work@gmail.com>
2023-09-05 16:56:49 +0400

    extract runner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,4 @@
-import algorithms.sort.mergesort.MergeSortBottomUp
-import algorithms.sort.mergesort.MergeSortRecursive
-import data.DataRepository
-import kotlin.system.measureTimeMillis
-
 fun main(args: Array<String>) {
-    val dataCount = 1_000_000
-    val runs = 100
 
-    var timeTotalRecursive = 0L
-    var timeTotalNonRecursive = 0L
-    repeat(runs) {
-        val recursiveTime = measureTimeMillis {
-            MergeSortRecursive().sort(DataRepository.getNumbers(count = dataCount))
-        }
-        timeTotalRecursive += recursiveTime
 
-        val nonRecursiveTime = measureTimeMillis {
-            MergeSortBottomUp().sort(DataRepository.getNumbers(count = dataCount))
-        }
-        timeTotalNonRecursive += nonRecursiveTime
-    }
-    val averageTimeRecursive = timeTotalRecursive / runs
-    val averageTimeNonRecursive = timeTotalNonRecursive / runs
-    println("MergeSort R, averageTime: $averageTimeRecursive")
-    println("MergeSort NR, averageTime: $averageTimeNonRecursive")
 }
