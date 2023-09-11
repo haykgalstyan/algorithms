0ecce23e67bd3a5550fc71e93c5a39788d6063b0
Hayk Galstyan <hayk.galstyan.work@gmail.com>
2023-09-11 17:12:59 +0400

    more

--- src/main/kotlin/ds/queue/BinaryHeapMaxPriorityQueue.kt
+++ src/main/kotlin/ds/queue/BinaryHeapMaxPriorityQueue.kt
@@ -19,7 +19,7 @@ class BinaryHeapMaxPriorityQueue<E>(
     }
 
     override fun peek(): E {
-        return array[pointer] as E
+        return get(pointer)
     }
 
     override fun isEmpty() = pointer == 0
@@ -31,16 +31,13 @@ class BinaryHeapMaxPriorityQueue<E>(
      * [enqueue] inserts a node at the end possibly causing a violation
      * of heap order, because the node can be larger than the parent.
      * The node needs to be promoted until the heap order is restored.
+     * (Bottom-up reheapification)
      */
-    @Suppress("UNCHECKED_CAST")
     private fun promoteNode() {
         var i = pointer
-        while (
-            i > 1 &&
-            comparator.compare(array[i] as E, array[i / 2] as E) > 0
-        ) {
-            val node = array[i] as E
-            val parent = array[i / 2] as E
+        while (i > 1 && comparator.compare(get(i), get(i / 2)) > 0) {
+            val node = get(i)
+            val parent = get(i / 2)
             array[i / 2] = node
             array[i] = parent
             i /= 2
@@ -50,11 +47,31 @@ class BinaryHeapMaxPriorityQueue<E>(
     /***
      * Demote the node until heap order is restored.
      *
-     * [enqueue] inserts a node at the end possibly causing a violation
-     * of heap order, because the node can be larger than the parent.
-     * The node needs to be promoted until the heap order is restored.
+     * [dequeue] exchanges the priority element with the end node and drops it
+     * causing a violation of heap order, because the node can be smaller than
+     * its child(ren).
+     * The node needs to be demoted until the heap order is restored.
+     * (Top-down reheapification)
      */
-    private fun demote(i: Int) {
+    private fun demote() {
+        var k = pointer
 
+        while (2 * k <= pointer) {
+
+            // pick the (smaller? larger?) child
+            var j = 2 * k
+            if (j < pointer && comparator.compare(get(j), get(j + 1)) < 0) j++
+
+            // check if done
+            if (comparator.compare(get(k), get(j)) > 0) break
+
+            // exchange k with j
+            // todo
+
+            k = j
+        }
     }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun get(index: Int) = array[index] as E
 }
