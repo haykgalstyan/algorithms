68c3dc1ad91fae45e63e0047f22734a296a324ca
haykgalstyan <hayk.galstyan.work@gmail.com>
2023-09-25 15:45:57 +0400

    binary search done

--- src/main/kotlin/algorithms/search/BinarySearch.kt
+++ src/main/kotlin/algorithms/search/BinarySearch.kt
@@ -1,21 +1,29 @@
 package algorithms.search
 
-fun <E> Array<E>.binarySearch(
+/***
+ * Basic binary search:
+ * Places a window of left and right indices on the [array].
+ * Then compares mid of the window to the search [element].
+ * Cuts the window from left or right depending on the comparison.
+ * Repeats until window size is 0 or the comparison results in a match.
+ */
+fun <E> binarySearch(
+    array: Array<E>,
     element: E,
     start: Int = 0,
-    end: Int = size - 1,
+    end: Int = array.size - 1,
     comparator: Comparator<E>,
-) {
-
+): Int {
     var l = start
     var r = end
 
     while (l <= r) {
         val m = (l + r) / 2
-        val mm = this[m]
+        val cmp = comparator.compare(array[m], element)
 
-        when (comparator.compare(mm, element)) {
-            1 ->
-        }
+        if (cmp < 0) l = m + 1
+        else if (cmp > 0) r = m - 1
+        else return m
     }
+    return -1
 }
---
+++ src/test/kotlin/algorithms/search/BinarySearchKtTest.kt
@@ -0,0 +1,31 @@
+package algorithms.search
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class BinarySearchKtTest {
+
+    private val input = arrayOf(
+        "A", "B", "C", "D", "E", "F", "G",
+    )
+
+    private val comparator = compareBy<String> { it }
+
+    @Test
+    fun `Test search works`() {
+        val expected = 3
+        val search = "D"
+        val actual = binarySearch(input, search, comparator = comparator)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Test not found returns invalid index`() {
+        val expected = -1
+        val search = "H"
+        val actual = binarySearch(input, search, comparator = comparator)
+
+        assertEquals(expected, actual)
+    }
+}
